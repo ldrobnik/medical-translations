a955569d71f91ef7de44ec2d236e83578fa38197
Łukasz Drobnik <drobniklukasz@gmail.com>
2020-05-03 21:23:07 +0200

    adjust background effect in sections

--- src/components/Expertise/Expertise.js
+++ src/components/Expertise/Expertise.js
@@ -17,7 +17,7 @@ const Expertise = (props) => {
 
         <Row
             id="expertise"
-            className="section noBackground">
+            className="section themeBackground">
             <h1>{WEBSITE_TEXT[state.language].expertise.title}</h1>
         </Row>
     );
